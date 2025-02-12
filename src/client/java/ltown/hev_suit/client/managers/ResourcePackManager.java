a2158786287a8da2d7677c0b9bb6e55f2d86fb7f
lunatic <loonietick@wii.com>
2025-02-12 02:46:04 -0500

    Update resource pack manager to allow user to disable it from settings

--- src/client/java/ltown/hev_suit/client/managers/ResourcePackManager.java
+++ src/client/java/ltown/hev_suit/client/managers/ResourcePackManager.java
@@ -5,25 +5,35 @@ import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.util.Identifier;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import net.minecraft.client.MinecraftClient;
 
 public class ResourcePackManager {
     private static final Logger LOGGER = LogManager.getLogger("ResourcePackManager");
+    private static final String PACK_ID = "hev_resources"; // pack identifier string
 
+    // Update activation type to DEFAULT_ENABLED
     public static void registerBuiltInResourcePack() {
         try {
             var modContainer = FabricLoader.getInstance()
                 .getModContainer("hev_suit")
                 .orElseThrow(() -> new RuntimeException("Could not find HEV Suit mod container"));
 
-                net.fabricmc.fabric.api.resource.ResourceManagerHelper.registerBuiltinResourcePack(
-                    Identifier.of("hev_suit", "hev_resources"),
-                    modContainer,
-                    ResourcePackActivationType.ALWAYS_ENABLED
-                );
-            
+            Identifier id = Identifier.of("hev_suit", PACK_ID);
+            net.fabricmc.fabric.api.resource.ResourceManagerHelper.registerBuiltinResourcePack(
+                id,
+                modContainer,
+                ResourcePackActivationType.DEFAULT_ENABLED
+            );
+            // Add the pack to client options if not already present
+            MinecraftClient client = MinecraftClient.getInstance();
+            if (!client.options.resourcePacks.contains(PACK_ID)) {
+                client.options.resourcePacks.add(PACK_ID);
+            }
             LOGGER.info("HEV Suit resource pack registered successfully");
         } catch (Exception e) {
             LOGGER.error("Failed to register HEV Suit resource pack", e);
         }
     }
+    
+
 }
